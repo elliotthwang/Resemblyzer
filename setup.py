71c268764e58a79f3b951aafe87b4b1e7602f074
elliotthwang <elliotthwang@gmail.com>
2020-10-03 11:14:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     version="0.1.1-dev",
     packages=find_packages(),
     package_data={
-        "resemblyzer": ["pretrained.pt"]
+        "resemblyzer": ["/content/drive/My Drive/TTS/中文 Pretrained/Encoder/ge2e_pretrained.pt"]
     },
     python_requires=">=3.5",
     install_requires=requirements,
     author="Corentin Jemine",
-    author_email="corentin@resemble.ai",
+    #author_email="corentin@resemble.ai",
     description="Analyze and compare voices with deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
